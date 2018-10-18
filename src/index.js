88055e37559bc32723fb75be20534d3ade818aeb
Rogelio Guzman <rogelioguzmanh@gmail.com>
2018-10-18 09:19:11 -0700

    Change to an async implementation and display feedback

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
-const execa = require('execa');
+import execa from 'execa';
+import ansiEscapes from 'ansi-escapes';
+import ora from 'ora';
 
 const NUM_RUNS = process.env.NUM_RUNS || 10;
 const JEST_BIN = process.env.JEST_BIN || 'node_modules/.bin/jest';
 
 const args = process.argv.slice(2);
-const times = (n, fn) => new Array(n).fill('').map(fn);
 
-const runTests = () => {
+const printOnCurrentLine = text => {
+  process.stdout.write(ansiEscapes.eraseLine);
+  process.stdout.write(ansiEscapes.cursorLeft);
+  process.stdout.write(text);
+};
+
+const runJest = async () =>
+  execa.stdout(JEST_BIN, [...args, '--useStderr', '--json']);
+
+const runTests = async ({
+  times,
+  results = [],
+  onTestRunCompleted = () => {},
+}) => {
+  if (times <= 0) {
+    return results;
+  }
   try {
-    const { stdout } = execa.sync(JEST_BIN, [...args, '--useStderr', '--json']);
+    const stdout = await runJest();
     const end = +new Date();
     const { startTime } = JSON.parse(stdout);
-    return (end - startTime) / 1000;
+    const duration = (end - startTime) / 1000;
+
+    onTestRunCompleted({ times });
+
+    return runTests({
+      times: times - 1,
+      results: results.concat([duration]),
+      onTestRunCompleted,
+    });
   } catch (e) {
     console.log(e);
-
     console.log(`
 jest-benchmarks is still really bad at reporing errors
  - Make sure that all the tests that you want to are passing
@@ -24,23 +48,35 @@ jest-benchmarks is still really bad at reporing errors
   }
 };
 
-console.log('Warming up cache...');
+const executeBenchmark = async () => {
+  const cacheSpinner = ora('Warming up cache').start();
+  // warm up cache
+  await runTests({ times: 2 });
+  cacheSpinner.succeed();
 
-// warm up cache
-times(2, runTests);
+  const testRunsSpinner = ora(`Test runs completed: 0/${NUM_RUNS}`).start();
+  const results = await runTests({
+    times: NUM_RUNS,
+    onTestRunCompleted: ({ times }) => {
+      testRunsSpinner.text = `Test runs completed: ${NUM_RUNS -
+        times +
+        1}/${NUM_RUNS}`;
+    },
+  });
 
-console.log('Running tests...');
+  testRunsSpinner.succeed();
 
-const results = times(NUM_RUNS, runTests);
+  const totalTime = results.reduce((sum, r) => sum + r, 0);
+  const average = (totalTime / NUM_RUNS).toFixed(3);
+  const max = results.reduce((max, r) => (r > max ? r : max), 0);
+  const min = results.reduce((min, r) => (r < min ? r : min), Infinity);
 
-const totalTime = results.reduce((sum, r) => sum + r, 0);
-const average = (totalTime / NUM_RUNS).toFixed(3);
-const max = results.reduce((max, r) => (r > max ? r : max), 0);
-const min = results.reduce((min, r) => (r < min ? r : min), Infinity);
+  console.log();
+  console.log(`Command: jest ${args.join(' ')}`);
+  console.log(`Number of runs: ${NUM_RUNS}`);
+  console.log(`Average: ${average}s`);
+  console.log(`Max: ${min}s`);
+  console.log(`Min: ${max}s`);
+};
 
-console.log();
-console.log(`Command: jest ${args.join(' ')}`);
-console.log(`Number of runs: ${NUM_RUNS}`);
-console.log(`Average: ${average}s`);
-console.log(`Max: ${min}s`);
-console.log(`Min: ${max}s`);
+executeBenchmark();
